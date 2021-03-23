68f501d118954b038aa2c07e2953f5fc14ea4968
humberto <humbertorivero9@gmail.com>
2021-03-23 00:22:13 -0300

    base de datos

--- Fundamentos/router/RutasWeb.js
+++ Fundamentos/router/RutasWeb.js
@@ -9,5 +9,9 @@ router.get('/servicios', (req, res) => {
     res.render("servicios", { tituloServicios: "Pagina de Servicios Dinamica" })
 });
 
+router.get('/template/mascotas', (req, res) => {
+    res.render("mascotas", { tituloServicios: "Pagina de Mascotas" })
+});
+
 module.exports = router;
 
